5697bab583bb23061f3e37906ec8f44ab87fd3ae
rbangamm <rbangamm@edu.uwaterloo.ca>
2018-02-24 10:17:07 -0500

    Changed return type from list to dict

--- summary.py
+++ summary.py
@@ -7,12 +7,11 @@ def summary(event, context):
 	summ_str = ""
 	code = []
 	strings = []
-	if (arr[3]):
-		if (len(arr[0]) == 0):
-			return arr[1]
-
+	counter = 0
 	for a in arr[0]:
 		#Assuming arr[2] is the title
 		summ_str = tt.summarize(arr[2], a)
+		strings.append(str(counter))
 		strings.append(summ_str)
-	return strings
+		counter += 1
+	return dict(zip(strings[::2], strings[1::2]))
