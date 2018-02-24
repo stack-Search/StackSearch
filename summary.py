a5b13122aff56f35321a86ddebcb1b4ea278a8b6
rbangamm <rbangamm@edu.uwaterloo.ca>
2018-02-24 12:41:49 -0500

    Created AWS lambda handler zip file

--- summary.py
+++ summary.py
@@ -1,11 +1,21 @@
 from textteaser import TextTeaser
 import json
 
-data = json.load(open('data.json'))
-tt = TextTeaser()
-for a in data["concept"]:
+def summary(event, context):
+	data = json.load(event)
+	tt = TextTeaser()
+	for x in range(len(data["concept"])):
+		data["concept"][x] = " ".join(tt.summarize(data["Title"], data["concept"][x]))
+	return data	
+
+#data = json.load(open('data.json'))
+#tt = TextTeaser()
+#for x in range(len(data["concept"])):
 	#Assuming arr[2] is the title
-	a = tt.summarize(data["Title"], a)
+#	print(data["concept"][x])
+#	print(type(data["concept"][x]))
+#	data["concept"][x] = " ".join(tt.summarize(data["Title"], data["concept"][x]))
+#	print(data["concept"][x])
 #str_dict = dict(zip(strings[::2], strings[1::2]))
-with open('data.json', 'w') as f:
-	json.dump(data, f, ensure_ascii=False)
+#with open('data.json', 'w') as f:
+#	json.dump(data, f, ensure_ascii=False)
